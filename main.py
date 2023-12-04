b96541eea7a9d5d2159440e178ce223084a8ed32
hawkCodes <92421892+hawk-Codes@users.noreply.github.com>
2023-12-04 18:18:20 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,43 @@
+import discord
+from discord.ext import commands
+import requests
+
+intents = discord.Intents.default()
+intents.typing = False
+intents.presences = False
+intents.message_content = True
+
+bot_token = "MTE4MDg2MDIyMDI5ODI0ODIxMg.GpnSdF.zirnTaxlFpogRkyS2QqLoWBjjWBxJphUYAmlZ4"
+github_access_token = "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"
+
+bot = commands.Bot(command_prefix="!", intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'Logged in as {bot.user.name} ({bot.user.id})')
+
+@bot.command()
+async def codegen(ctx, *, query):
+    try:
+        headers = {
+            "Authorization": f"token {github_access_token}"
+        }
+
+        url = f"https://api.github.com/search/code?q={query}"
+        response = requests.get(url, headers=headers)
+
+        if response.status_code == 200:
+            data = response.json()
+            items = data.get("items", [])
+            if items:
+                code_url = items[0].get("html_url", "")
+                await ctx.send(f"Here's a code example for '{query}':\n{code_url}")
+            else:
+                await ctx.send(f"No code examples found for '{query}'.")
+        else:
+            await ctx.send(f"An error occurred: {response.status_code}")
+
+    except Exception as e:
+        await ctx.send(f"An error occurred: {str(e)}")
+
+bot.run(bot_token)
